712ecd34009d06585c173418f6b344fd00a64512
YIGIT\Yigit <yigitburakkaymazsteam1234@gmail.com>
2021-06-20 20:00:09 +0300

    +added RotateSettings.cs

---
+++ Assets/Scripts/Lessons/RotateSettings.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Lessons
+{ 
+    [CreateAssetMenu(menuName = "Lessons/Lesson1/Rotate Settings")]
+    public class Rotate : ScriptableObject
+    {
+
+        [SerializeField] private float _speed = 1;  
+
+    
+    }
+}
